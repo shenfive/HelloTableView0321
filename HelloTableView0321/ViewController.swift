47daba0194b84dce9d5778c0e49f5ae1c6c633ae
Danny Shen <shenfive@gmail.com>
2020-03-21 13:25:34 +0800

    tabelView Init

--- HelloTableView0321/ViewController.swift
+++ HelloTableView0321/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelegate{
 
+
+    @IBOutlet weak var theTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        theTableView.dataSource = self
+        theTableView.delegate = self
+        
     }
 
-
+    //MARK: TableView Data Source & Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
 }
 
